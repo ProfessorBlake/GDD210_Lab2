e06a758c58642bbcb439aa14f9df4d617491a739
Blake <telefragent@gmail.com>
2019-08-25 18:58:41 -0400

    Added scene 2 and 3

---
+++ Assets/Game/Scripts/1.Physics Intro/ResetOnClick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This class is designed to be reusable, so that any gameobject we attach it to will reset its position and velocity when we click.
+//Think about writing reusable code when you work, it saves time and is usually more effecient.
+public class ResetOnClick : MonoBehaviour
+{
+	public Rigidbody2D Rigidbody;
+
+	private Vector2 startPosition;
+
+	private void Start()
+	{
+		startPosition = transform.position;
+	}
+
+	private void Update()
+    {
+		//Reset this objects position and velocity when the mouse is clicked.
+		if (Input.GetMouseButtonDown(0))
+		{
+			transform.position = startPosition;
+			Rigidbody.velocity = Vector2.zero;
+			Rigidbody.angularVelocity = 0f;
+		}
+    }
+}
---
+++ Assets/Game/Scripts/3.Physics Forces/ForceCreator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceCreator : MonoBehaviour
+{
+	public float ForceStrength;
+	public Rigidbody2D[] SceneRigidbodies; //An array of the RigidBody2Ds in the scene.
+
+	private void Update()
+	{
+		//If we right click, apply a vertical force to our rigidbodies.
+		if(Input.GetMouseButtonDown(1))
+		{
+			for(int i = 0; i < SceneRigidbodies.Length; i++)
+			{
+				SceneRigidbodies[i].AddForce(Vector2.up * ForceStrength);
+			}
+		}
+	}
+}
