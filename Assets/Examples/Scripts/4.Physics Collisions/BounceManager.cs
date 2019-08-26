34e56466ab31ca3a13ab55c6d7d768f0f1f9cba4
Blake <telefragent@gmail.com>
2019-08-26 16:02:30 -0400

    Scene 4 stuff

--- Assets/Examples/Scripts/4.Physics Collisions/BounceManager.cs
+++ Assets/Examples/Scripts/4.Physics Collisions/BounceManager.cs
@@ -6,6 +6,8 @@ public class BounceManager : MonoBehaviour
 	public Text BounceText;
 	public int BounceCount;
 
+	//Public functions can be called from other scripts, as long as they have a reference to an instance of this class.
+	//In AddBounce, we take an int as a parameter, and add that to our BounceCount variable.
 	public void AddBounce(int bounces)
 	{
 		BounceCount += bounces;
--- Assets/Examples/Scripts/4.Physics Collisions/CollisionTester.cs
+++ Assets/Examples/Scripts/4.Physics Collisions/CollisionTester.cs
@@ -4,8 +4,9 @@ public class CollisionTester : MonoBehaviour
 {
 	public BounceManager BounceManager;
 
-	//Any collider2D component will call this function on
-	//any attached scripts.
+	//Any Collider2D component will call this function on
+	//any attached scripts when the collider enters a collision with another collider.
+	//The gameobject must also have a RigidBody2D attached.
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
 		BounceManager.AddBounce(1);
