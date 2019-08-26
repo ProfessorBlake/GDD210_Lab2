dcdb9e1f4df835449ce3008395a6773a2381b96c
Blake <telefragent@gmail.com>
2019-08-26 14:58:09 -0400

    Added fixedupdate example

--- Assets/Examples/Scripts/3.Physics Forces/ForceCreator.cs
+++ Assets/Examples/Scripts/3.Physics Forces/ForceCreator.cs
@@ -10,12 +10,26 @@ public class ForceCreator : MonoBehaviour
 	private void Update()
 	{
 		//If we right click, apply a vertical force to our rigidbodies.
+		//Adding single impulses can be done in Update.
 		if(Input.GetMouseButtonDown(1))
 		{
 			Debug.Log("Adding force!");
 			for(int i = 0; i < SceneRigidbodies.Length; i++)
 			{
 				SceneRigidbodies[i].AddForce(Vector2.up * ForceStrength);
+			}
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		//If we hold space, add a force opposite of gravity * mass. This should negate gravity and cause the rigidbody to float.
+		//Any constant change added to physics should be done in FixedUpdate.
+		if(Input.GetKey(KeyCode.Space))
+		{
+			for (int i = 0; i < SceneRigidbodies.Length; i++)
+			{
+				SceneRigidbodies[i].AddForce(SceneRigidbodies[i].mass * -Physics2D.gravity);
 			}
 		}
 	}
