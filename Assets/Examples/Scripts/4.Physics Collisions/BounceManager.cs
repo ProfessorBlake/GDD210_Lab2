b89de423c78e74e7fb3d8b97914c12e277767966
Blake <telefragent@gmail.com>
2019-08-26 15:41:06 -0400

    Added bounce counter

---
+++ Assets/Examples/Scripts/4.Physics Collisions/BounceManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BounceManager : MonoBehaviour
+{
+	public Text BounceText;
+	public int BounceCount;
+
+	public void AddBounce(int bounces)
+	{
+		BounceCount += bounces;
+		BounceText .text= "Bounces" + BounceCount.ToString();
+	}
+}
---
+++ Assets/Examples/Scripts/4.Physics Collisions/CollisionTester.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CollisionTester : MonoBehaviour
+{
+	public BounceManager BounceManager;
+
+	//Any collider2D component will call this function on
+	//any attached scripts.
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		BounceManager.AddBounce(1);
+	}
+}
