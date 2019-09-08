9c67075225db071d610e1f220dc736cf360197b6
Blake <telefragent@gmail.com>
2019-09-08 19:09:23 -0400

    Rope

--- Assets/Examples/Scripts/ADVANCED/Rope.cs
+++ Assets/Examples/Scripts/ADVANCED/Rope.cs
@@ -18,6 +18,8 @@ public class Rope : MonoBehaviour
 
 	private void FixedUpdate()
 	{
+		//Because the line renderer uses an array of Vector3 for it's position data, we need to update them whenever our gameobject moves.
+		//Since our crates are physics objects, they'll only move on FixedUpdate frames, so we don't need to change their position in Update.
 		Line.SetPositions(new Vector3[]
 			{
 				transform.position,
